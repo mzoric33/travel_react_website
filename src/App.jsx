3b8756c18e4e41fecbcc516d915da30604fc2aaa
mzoric33 <zoricmarin4@gmail.com>
2022-10-11 20:56:01 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <h1>YO</h1>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
