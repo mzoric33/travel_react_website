81ca886cf0756fd903f151bc130d4ce1cd539f45
mzoric33 <zoricmarin4@gmail.com>
2022-10-13 20:18:23 +0200

    hero_section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
+        <HeroSection></HeroSection>
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" exact component={Home} />
         </Routes>
       </Router>
     </>
